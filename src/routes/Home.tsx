fa324dc833b49c05fec35128462f26c22cdcb5f8
antonio.luciano <antonio.luciano@hdtecnologia.com.br>
2025-04-23 09:02:42 -0300

    versão 1.0.0

---
+++ src/routes/Home.tsx
@@ -0,0 +1,35 @@
+import { UserProps } from "../types/user";
+
+import { useState } from "react"
+import Search from "../components/Search"
+
+const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadeUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+
+    const data = await res.json();
+
+    const { avatar_url, login, location, followers, following} = data;
+
+    const userData: UserProps = {
+      avatar_url, 
+      login, 
+      location, 
+      followers, 
+      following,
+    };
+
+    setUser(userData);
+  };
+
+  return (
+    <div>
+      <Search loadUser={loadeUser}/>
+      {user && <p>{user.login}</p>}
+    </div>
+  )
+}
+
+export default Home
