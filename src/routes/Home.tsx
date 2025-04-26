a530568d98d3b106379e71aedd1237ae634cfa9b
antonio.luciano <antonio.luciano@hdtecnologia.com.br>
2025-04-26 09:20:10 -0300

    versão 1.0.1

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,29 @@
 import { UserProps } from "../types/user";
 
-import { useState } from "react"
-import Search from "../components/Search"
+import Search from "../components/Search";
+import User from '../components/User';
+import Error from "../components/Error";
+
+import { useState } from "react";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState (false);
 
   const loadeUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+    
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
 
     const data = await res.json();
+
+    if (res.status === 404){
+      setError(true);
+      return;
+
+    }
 
     const { avatar_url, login, location, followers, following} = data;
 
@@ -23,11 +37,12 @@ const Home = () => {
 
     setUser(userData);
   };
-
+// O {...user} sped operations que é meu state
   return (
     <div>
       <Search loadUser={loadeUser}/>
-      {user && <p>{user.login}</p>}
+      {user && <User {...user}/>}
+      {error && <Error /> }
     </div>
   )
 }
